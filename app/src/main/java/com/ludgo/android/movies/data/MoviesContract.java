0fac18f77674c16338d155cc1b2c88c4b34ae34a
ludgo <ludgogit@gmail.com>
2015-08-23 09:50:56 +0200

    feat: database contract with single table

---
+++ app/src/main/java/com/ludgo/android/movies/data/MoviesContract.java
@@ -0,0 +1,58 @@
+package com.ludgo.android.movies.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Defines tables and column names for the movies database.
+ */
+public final class MoviesContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public MoviesContract() {}
+
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "com.ludgo.android.movies";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+    public static final String PATH_MOVIE = "movie";
+
+    /* Inner class that defines the table contents of the movies table */
+    public static final class MoviesEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        // URI containing a Cursor of zero or more items.
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        // URI containing a Cursor of a single item.
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
+        public static final String TABLE_NAME = "movies";
+
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_POPULARITY = "popularity";
+
+        // @return Uri like content://com.ludgo.android.movies/movie/{id}
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
